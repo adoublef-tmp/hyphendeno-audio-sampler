482cff424d63b5011d21ab7f66b454353af8e9aa
topheruk <kristopherab@gmail.com>
2022-07-28 13:10:10 +0000

    drag&drop on sample pad

--- islands/AudioSampler.tsx
+++ islands/AudioSampler.tsx
@@ -1,10 +1,11 @@
 /** @jsx h */
 import { createContext, h } from "preact";
-import { useContext, useReducer } from "preact/hooks";
+import { useContext, useReducer, useState } from "preact/hooks";
+import { DBSchema, openDB } from "idb";
 import { tw } from "@twind";
 
-export default function AudioSampler({ dbName, version }: { dbName: string, version: number; }) {
-    const AudioSamplerContext = wrapper({ dbName, version });
+export default function AudioSampler({ dbName, version }: AudioSamplerProps) {
+    const AudioSamplerContext = setupAudioSamplerContext({ dbName, version });
 
     return (
         <AudioSamplerContext>
@@ -14,20 +15,94 @@ export default function AudioSampler({ dbName, version }: { dbName: string, vers
 }
 
 function useTestButton() {
-    const { dispatch, state } = useAudioSamplerContext();
+    const { dispatch, state: { dbName, version } } = useAudioSamplerContext();
+    const [name, setName] = useState("todo");
 
-    return { dispatch, state };
+    const connectDB = async () => {
+        const db = await dbConnection({ dbName, version });
+
+        // const tx = db.transaction("sample", "readwrite");
+
+        // await tx.done;
+
+        console.log(`connecting to v${version} of ${dbName}`, db);
+    };
+
+    function uploadSample(e: DragEvent) {
+        e.preventDefault();
+
+        // only one file at a time
+        const file = e.dataTransfer?.files.item(0);
+
+        if (file) {
+            // remove extension from filename and set to name
+            const name = file?.name.replace(/\.[^/.]+$/, "");
+            setName(name);
+        }
+    }
+
+    return { connectDB, uploadSample, name };
 }
 
 function TestButton() {
-    const { dispatch, state } = useTestButton();
+    const { connectDB, uploadSample, name } = useTestButton();
 
     return (
-        <button onClick={() => console.log(state.dbName, state.version)}>
-            Test
+        <button
+            class={tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+            onPointerDown={connectDB}
+            onDrop={uploadSample}
+            onDragOver={(e) => e.preventDefault()}
+        >
+            {name}
         </button>
     );
 }
+
+/** IndexedDB */
+
+type AudioSamplerSchema = DBSchema & {
+    sample: {
+        key: string;
+        value: SampleEntry;
+    };
+    user: {
+        key: string;
+        value: UserEntry;
+    };
+};
+
+type SampleEntry = {
+    id: number;
+    name: string;
+    file: File;
+};
+
+type UserEntry = {
+    id: number;
+    name: string;
+};
+
+function dbConnection({ dbName, version }: { dbName: string, version: number; }) {
+    return openDB<AudioSamplerSchema>(dbName, version, {
+        upgrade: (db, prev, curr, tx) => {
+            console.log(`${db} upgraded from v${prev} to v${curr}`);
+
+            if (db.objectStoreNames.contains("sample"))
+                db.deleteObjectStore("sample");
+
+            if (db.objectStoreNames.contains("user"))
+                db.deleteObjectStore("user");
+
+            db.createObjectStore("sample", { keyPath: "id", autoIncrement: true });
+            db.createObjectStore("user", { keyPath: "id", autoIncrement: true });
+        }
+    });
+}
+
+/** Audio Sampler */
+
+type AudioSamplerProps = { dbName: string, version: number; };
 
 type AudioSamplerAction = | { type: "test"; payload: AudioSamplerState; };
 
@@ -42,16 +117,6 @@ const AudioSamplerDispatchContext = createContext<AudioSamplerDispatch>(() => { 
 
 const AudioSamplerStateContext = createContext<AudioSamplerState | undefined>(undefined);
 
-const audioSamplerReducer = (state: AudioSamplerState, action: AudioSamplerAction) => {
-    switch (action.type) {
-        case "test":
-            console.log("test");
-            return state;
-        default:
-            return state;
-    }
-};
-
 function useAudioSamplerContext() {
     const dispatch = useContext(AudioSamplerDispatchContext);
     const state = useContext(AudioSamplerStateContext);
@@ -61,9 +126,20 @@ function useAudioSamplerContext() {
     return { dispatch, state };
 }
 
-function wrapper({ dbName, version }: { dbName: string, version: number; }) {
+function setupAudioSamplerContext({ dbName, version }: { dbName: string, version: number; }) {
+    // const db = connectDB({ dbName, version });
+
+    const [state, dispatch] = useReducer<AudioSamplerState, AudioSamplerAction>((state, action) => {
+        switch (action.type) {
+            case "test":
+                console.log("test");
+                return state;
+            default:
+                return state;
+        }
+    }, { dbName, version });
+
     return function ({ children }: { children: h.JSX.Element; }) {
-        const [state, dispatch] = useReducer(audioSamplerReducer, { dbName, version });
         return (
             <AudioSamplerStateContext.Provider value={state}>
                 <AudioSamplerDispatchContext.Provider value={dispatch}>
