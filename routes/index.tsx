108fb8557c43ddacbee412397c73a6d9f67c3094
topheruk <kristopherab@gmail.com>
2022-07-29 13:16:41 +0000

    added fresh link to index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <main>
             <h1>Audio Sampler Project!</h1>
-            <p>This demo project uses Deno's new web framework Fresh</p>
+            <p>This demo project uses Deno's new web framework <a href="https://fresh.deno.dev/">Fresh</a></p>
             <AudioSampler dbName={"audio_sampler"} version={1} />
         </main>
     );
